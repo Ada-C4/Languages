4fed17dfcf3c90eb4b53f4b56c60d3f0f428cc72
Brittany <BrittanyKohler@gmail.com>
2016-04-04 10:24:17 -0700

    Adding score and check methods in python

--- planet-express/PlanetExpress.py
+++ planet-express/PlanetExpress.py
@@ -5,23 +5,48 @@ class PlanetExpress:
         self.receipts = 50
         self.horde = 50
         self.hunger = 50
+        self.stable = True
 
     def drink():
-        self.thirst -= 5
-        self.work += 5
+        self.thirst -= 4
+        self.work += 4
 
     def deliver():
-        self.receipts += 5
-        self.work -= 5
+        self.receipts += 4
+        self.work -= 4
 
     def steal():
-        self.horde += 5
-        self.work += 5
+        self.horde += 4
+        self.work += 4
 
     def eat():
-        self.hunger -= 5
-        self.work += 5
+        self.hunger -= 4
+        self.work += 4
 
     def account():
-        self.receipts -= 5
-        self.horde -= 5
+        self.receipts -= 4
+        self.horde -= 4
+
+    def score():
+        horde_score = self.horde * 1000
+        thirst_score = self.thirst * -10
+        receipts_score = self.receipts * -10
+        work_score = self.work * -10
+        hunger_score = self.hunger * -10
+        return horde_score + thirst_score + receipts_score + work_score + hunger_score
+
+
+    def check():
+        if (self.thirst >= 90 and self.thirst < 100):
+            print "Warning! Fry is very thirsty and is in danger of crashing the ship."
+        if (self.work >= 90 and self.work < 100):
+            print "Warning! Leela has tons of work and is in danger of crashing the ship."
+        if (self.horde <= 10 and self.work > 0):
+            print "Warning! Bender's horde is very low and is in danger of crashing the ship."
+        if (self.hunger >= 90 and self.hunger < 100):
+            print "Warning! Zoidberg is very hungry and is in danger of crashing the ship."
+        if (self.receipts >= 90 and self.receipts < 100):
+            print "Warning! Hermes' backlog of receipts is huge and is in danger of crashing the ship."
+        if (self.thirst < 100 and self.work < 100 and self.hunger < 100 and self.horde > 0 and self.receipts < 100):
+            self.stable = false
+        return self.stable
