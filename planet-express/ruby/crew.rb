ba5ba76a06e5bd9e91b31449037cadf47c554fc3
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-04-04 12:13:14 -0700

    created ruby class skeleton

---
+++ planet-express/ruby/crew.rb
@@ -0,0 +1,34 @@
+class Crew
+  attr_accessor :thirst, :work, :horde, :hunger, :receipts, :stable
+
+  def initialize
+    @thirst = 50
+    @work = 50
+    @horde = 50
+    @hunger = 50
+    @receipts = 50
+    @stable = true
+  end
+
+  def drink
+  end
+
+  def deliver
+  end
+
+  def steal
+  end
+
+  def eat
+  end
+
+  def account
+  end
+
+  def score
+  end
+
+  def check
+  end
+
+end
