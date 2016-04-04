0aef5e9b3b662dfbff1ce383c8064b4a799a40ba
Brittany <BrittanyKohler@gmail.com>
2016-04-04 09:44:11 -0700

    Adding constructor and basic methods in python

---
+++ planet-express/PlanetExpress.py
@@ -0,0 +1,27 @@
+class PlanetExpress:
+    def __init__:
+        self.thirst = 50
+        self.work = 50
+        self.receipts = 50
+        self.horde = 50
+        self.hunger = 50
+
+    def drink():
+        self.thirst -= 5
+        self.work += 5
+
+    def deliver():
+        self.receipts += 5
+        self.work -= 5
+
+    def steal():
+        self.horde += 5
+        self.work += 5
+
+    def eat():
+        self.hunger -= 5
+        self.work += 5
+
+    def account():
+        self.receipts -= 5
+        self.horde -= 5
