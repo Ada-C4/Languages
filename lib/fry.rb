4586660173796aa191748704ddb1e4dd3baf9cd4
Amy Hunter <amyrhunter@gmail.com>
2016-04-04 10:58:01 -0700

    Delete duplicate fry and game files

---
+++ lib/fry.rb
@@ -0,0 +1,8 @@
+class Fry
+
+  attr_accessor :name, :thirst
+
+  def initialize
+    @name = "Fry"
+    @thirst = 50
+  end
--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 module PlanetExpress
   class Game
-    attr_accessor: :bender, :fry, :hermes, :leela, :zoidberg
+    attr_accessor :bender, :fry, :hermes, :leela, :zoidberg
 
     def initialize
       @bender = Bender.new
@@ -8,4 +8,16 @@ module PlanetExpress
       @hermes = Hermes.new
       @leela = Leela.new
       @zoidberg = Zoidberg.new
+      @stable = true
     end
+
+    def score
+
+    end
+
+    def check
+      if @fry.thirst > 95 || @leela.work > 95 || @bender.horde < 5 || @zoidberg.hunger > 95 || @hermes.receipts < 5
+        @stable = false
+    end
+  end
+end
