f4ce702debf6ace7b1bb4b27b584a6d81b5ee385
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-04-06 14:15:48 -0700

    started adding python crew class

---
+++ planet-express/python/crew.py
@@ -0,0 +1,15 @@
+class Crew:
+    'defines some properties about the crew of the spaceship'
+
+    def __init__(self):
+        self.thirst = 50
+        self.work = 50
+        self.horde = 50
+        self.hunger = 50
+        self.receipts = 50
+        self.stable = true
+        self.score = 50
+
+    def drink:
+        self.thirst -= 5
+        self.work += 5
