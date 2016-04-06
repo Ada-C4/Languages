ba3dfa16eb99130edecbf55b3242bd1dea38e7be
Kelly <kelly_devlin@alumni.brown.edu>
2016-04-06 09:41:19 -0700

    update prompt

--- poverty/src/poverty/Program.java
+++ poverty/src/poverty/Program.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Program {
 	public static void main(String[] args) {	
-		System.out.println("Welcome to Poverty Educator!");
-		System.out.print("Enter a U.S. county to learn about: ");
+		System.out.println("Welcome to CA/WA Poverty Educator!");
+		System.out.print("Enter a county in either California or Washington: ");
 		Scanner scan = new Scanner(System.in);
 		String county = scan.nextLine();
 		System.out.println("County: "+ county);
