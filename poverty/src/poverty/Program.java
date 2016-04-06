69dd1c4983078b824c400e22d6debb1f530bed26
Kelly <kelly_devlin@alumni.brown.edu>
2016-04-06 09:32:09 -0700

    program captures user input

--- poverty/src/poverty/Program.java
+++ poverty/src/poverty/Program.java
@@ -1,10 +1,14 @@
 package poverty;
 
+import java.util.Scanner;
+
 public class Program {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) {	
+		System.out.println("Welcome to Poverty Educator!");
+		System.out.print("Enter a U.S. county to learn about: ");
+		Scanner scan = new Scanner(System.in);
+		String county = scan.nextLine();
+		System.out.println("County: "+ county);
+		scan.close();
 	}
-
 }
