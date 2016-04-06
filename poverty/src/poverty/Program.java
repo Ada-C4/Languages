fcac531d6ff405bf33f00f72a447f15355cb447d
Kelly <kelly_devlin@alumni.brown.edu>
2016-04-06 14:17:42 -0700

    add california txt file, user can choose state and county and program prints line from txt file if county is found

--- poverty/src/poverty/Program.java
+++ poverty/src/poverty/Program.java
@@ -7,18 +7,27 @@ import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {	
 		System.out.println("Welcome to CA/WA Poverty Educator!");
-		System.out.print("Enter a county in either California or Washington: ");
 		Scanner scan = new Scanner(System.in);
-		String county = scan.nextLine();
-		System.out.println("County: "+ county);
-		scan.close();
-		
-		File washington = new File("washington.txt");
-		
+		// capture user input for state 
+		System.out.print("Enter 'CA' for California or 'WA' for Washington: ");
+		String state = scan.nextLine().toUpperCase();
+		// use appropriate file based on user state selection
+		File file = null; 
+		if(state.equals("WA")) {
+			file = new File("washington.txt");
+		} else if(state.equals("CA")){
+			file = new File("california.txt");
+		}
 		try {
-			Scanner scanner = new Scanner(washington);
+			Scanner scanner = new Scanner(file);
+			// capture user input for county 
+			System.out.print("Enter a county in " + state + ": ");
+			String county = scan.nextLine();
+			scan.close();
+			// find and print the line matching the county entered by user
 			int lineNum = 0;
 			String line; 
+			boolean no_county = true;
 			while (scanner.hasNextLine()) {
 				line = scanner.nextLine();
 				lineNum++;
@@ -26,9 +35,13 @@ public class Program {
 				if(line.toLowerCase().contains(county.toLowerCase())) { 
 					System.out.println("Line num: " + lineNum);
 		            System.out.println("Line contents: " + line);
+		            no_county = false;
 		        }
 			}
 		    scanner.close();
+		    if(no_county) {
+		    	System.out.println("Invalid county for " + state);
+		    }
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
