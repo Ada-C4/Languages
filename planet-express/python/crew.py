a61623e70d834e703664b7101e945b16c7016f69
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-04-06 14:36:11 -0700

    added python check method

--- planet-express/python/crew.py
+++ planet-express/python/crew.py
@@ -34,4 +34,26 @@ class Crew:
         if self.thirst >= 100 or self.work >= 100 or self.horde <= 0 or self.hunger >= 100 or self.receipts <= 0:
             self.score = 0
 
-    
+    def check:
+        # if score is too low, end the game
+        if self.score <= 0:
+            self.stable = false
+            return true
+        else:
+            # check to see if you need to print warnings
+            if self.thirst >= 85:
+                print "Fry is scary thirsty!"
+
+            if self.work >= 85:
+                print "Leela is exhausted from all this work!"
+
+            if self.horde <= 15:
+                print "Bender is sad because he doesn't have enough treasure!"
+
+            if self.hunger >= 85:
+                print "Zoidberg is going to eat something and it's not going to be pretty!"
+
+            if self.receipts <= 15:
+                print "Hermes doesn't have enough receipts!"
+
+            return false
