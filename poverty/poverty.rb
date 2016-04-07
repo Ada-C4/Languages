c0fc2ab35c30f5c6246f7c526939128562f75688
Rebecca Tolmach <rmtolmach@gmail.com>
2016-04-07 10:45:13 -0700

    finding lowest and higest poverty in WA

--- poverty/poverty.rb
+++ poverty/poverty.rb
@@ -12,57 +12,42 @@ class County
     file_WA = File.open('est14_WA.txt')
     data_array = []
     file_WA.each do |line|
-      data_array.push(County.new(line[193..237], line[76..79], line[49..56], line[133..138]))
+      data_array.push(County.new(line[193..217], line[76..79], line[49..56], line[133..138]))
     end
     return data_array
   end
-  # puts self.all_counties[3].name
-  # puts self.all_counties[3].percent
-  # puts self.all_counties[3].num
-  # puts self.all_counties[3].median_income
 
   def self.find_highest_pov
     highest = 0
     county_name = ""
+    count = 0
+    income = 0
     all_counties.each do |county|
       if county.percent > highest
         highest = county.percent
         county_name = county.name
+        count = county.num
+        income = county.median_income
       end
     end
-    return "highest is #{highest} and the name is #{county_name}"
-
+    return "#{highest}% poverty in #{county_name}. Count is #{count} and median income is $#{income}"
   end
   puts self.find_highest_pov
 
+  def self.find_lowest_pov
+    lowest = 100
+    county_name = ""
+    count = 0
+    income = 0
+    all_counties.each do |county|
+      if county.percent < lowest
+        lowest = county.percent
+        county_name = county.name
+        count = county.num
+        income = county.median_income
+      end
+    end
+    return "#{lowest}% poverty in #{county_name}. Count is #{count} and median income is $#{income}"
+  end
+  puts self.find_lowest_pov
 end
-
-
-# @file_WA = File.read('est14_WA.txt')
-# @file_CA = File.read('est14_WA.txt')
-#
-# data_array = []
-#   @file_WA.each do |line|
-#     data_array.push(line)
-#   end
-#
-#   d = data_array[1].split(' ')
-#
-#   if d[76..80] == "32.2"
-#     puts d[23]
-#   end
-#   puts d[2]
-#   puts d.class
-#   puts d[23..24].join(' ')
-
-
-# def highest_percent_poverty
-#   percent_array = []
-#   @file_WA.each do |line|
-#     percent_array.push(line.slice(76..80))
-#   end
-#   return percent_array.max
-# end
-
-# puts highest_percent_poverty
-# puts find_county
