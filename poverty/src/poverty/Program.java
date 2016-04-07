e1a47bc0be14535fc64330e7cf60b65aff3e156d
Kelly <kelly_devlin@alumni.brown.edu>
2016-04-07 09:58:51 -0700

    user input for state in a while loop

--- poverty/src/poverty/Program.java
+++ poverty/src/poverty/Program.java
@@ -8,47 +8,48 @@ public class Program {
 	public static void main(String[] args) {	
 		System.out.println("Welcome to the CA/WA Poverty Educator!");
 		Scanner scan = new Scanner(System.in);
-		// capture user input for state 
-		System.out.print("Enter 'CA' for California or 'WA' for Washington: ");
-		String state = scan.nextLine().toUpperCase();
-		// use appropriate file based on user state selection
+
 		File file = null; 
-		if(state.equals("WA")) {
-			file = new File("washington.txt");
-		} else if(state.equals("CA")){
-			file = new File("california.txt");
-		}
-		if(file != null) {
-			try {
-				Scanner scanner = new Scanner(file);
-				// capture user input for county 
-				System.out.print("Enter a county in " + state + ": ");
-				String county = scan.nextLine();
-				scan.close();
-				// find and print the line matching the county entered by user
-				String line; 
-				boolean countyFound = false;
-				while (scanner.hasNextLine()) {
-					line = scanner.nextLine();
-					
-					if(line.toLowerCase().contains(county.toLowerCase())) { 		            
-			            County userCounty = new County(line);
-			            System.out.println("County: " + userCounty.name );
-			            System.out.println("Percentage of children in poverty: " + userCounty.povPercent);
-			            System.out.println("Number of children in poverty: " + userCounty.povCount);
-			            System.out.println("Median household income: "+ userCounty.medIncome);
-			            countyFound = true;
-			        }
-				}
-			    scanner.close();
-			    if(!countyFound) {
-			    	System.out.println("Invalid county for " + state);
-			    }
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
+		String state = null;
+		while(file == null) {
+			// capture user input for state 
+			System.out.print("Enter 'CA' for California or 'WA' for Washington: ");
+			state = scan.nextLine().toUpperCase();
+			// use appropriate file based on user state selection
+			if(state.equals("WA")) {
+				file = new File("washington.txt");
+			} else if(state.equals("CA")){
+				file = new File("california.txt");
 			}
-		} else {
-			System.out.println("Invalid state.");
+		}	
+		
+		try {
+			Scanner scanner = new Scanner(file);
+			// capture user input for county 
+			System.out.print("Enter a county in " + state + ": ");
+			String county = scan.nextLine();
+			scan.close();
+			// find and print the line matching the county entered by user
+			String line; 
+			boolean countyFound = false;
+			while (scanner.hasNextLine()) {
+				line = scanner.nextLine();
+				
+				if(line.toLowerCase().contains(county.toLowerCase())) { 		            
+		            County userCounty = new County(line);
+		            System.out.println("County: " + userCounty.name );
+		            System.out.println("Percentage of children in poverty: " + userCounty.povPercent);
+		            System.out.println("Number of children in poverty: " + userCounty.povCount);
+		            System.out.println("Median household income: "+ userCounty.medIncome);
+		            countyFound = true;
+		        }
+			}
+		    scanner.close();
+		    if(!countyFound) {
+		    	System.out.println("Invalid county for " + state);
+		    }
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 		
 	}
