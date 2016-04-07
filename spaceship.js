16ba271727b024f71401673a3aab57b9ab1fecbf
Sarah <trowbrsa@gmail.com>
2016-04-07 12:22:31 -0700

    Added case for ending game

--- spaceship.js
+++ spaceship.js
@@ -1,38 +1,38 @@
 var SpaceShip = function(start, end) {
- this.thirst = start;
- this.work = start;
- this.horde = start;
- this.hunger = start;
- this.receipts = start;
- this.stable = true;
- this.end = end;
- this.totalScore = 0;
+  this.thirst = start;
+  this.work = start;
+  this.horde = start;
+  this.hunger = start;
+  this.receipts = start;
+  this.stable = true;
+  this.end = end;
+  this.totalScore = 0;
 }
 
 SpaceShip.prototype.drink = function(){
- this.thirst--;
- this.work++;
+  this.thirst--;
+  this.work++;
 };
 
 SpaceShip.prototype.deliver = function(){
- this.work--;
- this.receipts ++;
+  this.work--;
+  this.receipts ++;
 };
 
 SpaceShip.prototype.steal = function(){
- this.horde++;
- this.work++;
+  this.horde++;
+  this.work++;
 };
 
 SpaceShip.prototype.eat = function(){
- this.hunger--;
- this.work++;
+  this.hunger--;
+  this.work++;
 };
 
 SpaceShip.prototype.account = function(){
- this.receipts--;
- this.horde--;
- $("#receipts").text(crew.receipts);
+  this.receipts--;
+  this.horde--;
+  $("#receipts").text(crew.receipts);
 };
 
 SpaceShip.prototype.status = function(){
@@ -44,39 +44,50 @@ SpaceShip.prototype.status = function(){
 };
 
 SpaceShip.prototype.score = function(){
-    if (this.thirst > 70) {
-      this.totalScore --;
-    } else if (this.thirst <= 70) {
-      this.totalScore ++;
-    }
-
-    if (this.work > 70) {
-      this.totalScore --;
-    } else if (this.work <= 70) {
-      this.totalScore ++;
-    }
-
-    if (this.receipts > 70) {
-      this.totalScore ++;
-    } else if (this.receipts <= 80) {
-      this.totalScore --;
-    }
-
-  if (this.horde > 70) {
-    this.totalScore ++;
-  } else if (this.horde <= 70) {
-    this.totalScore ++;
+  if (this.totalScore < 0) {
+    alert("You lost! Try again.");
+    location.reload();
   }
 
-  if (this.thirst > 70) {
+  if (this.thirst > 70 && this.thirst < this.end) {
     this.totalScore --;
   } else if (this.thirst <= 70) {
     this.totalScore ++;
+  } else if (this.thirst >= this.end) {
+    alert("Your crew got too thirsty! You lose - try again.");
+    location.reload();
   }
+
+  if (this.work > 70) {
+    this.totalScore --;
+  } else if (this.work <= 70 && this.work < this.end) {
+    this.totalScore ++;
+  } else if (this.work >= this.end) {
+    alert("Your crew got too hungry! You lose - try again.");
+    location.reload();
+  }
+
+  if (this.receipts > 70) {
+    this.totalScore ++;
+  } else if (this.receipts <= 80 && this.receipts < this.end) {
+    this.totalScore --;
+  } else if (this.receipts >= this.end) {
+    alert("Your crew got too behind on receipts! You lose - try again.");
+    location.reload();
+}
+
+  if (this.horde > 70) {
+    this.totalScore ++;
+  } else if (this.horde <= 70 && this.horde < this.end) {
+    this.totalScore ++;
+  } else if (this.horde >= this.end) {
+    alert("Your crew got too behind on receipts! You lose - try again.");
+    location.reload();
+}
   $("#score").text(this.totalScore);
 };
 
-var crew = new SpaceShip(50, 100);
+var crew = new SpaceShip(0, 3);
 
 $(document).ready(function(){
   crew.status();
