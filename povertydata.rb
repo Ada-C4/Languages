19b200e3dcdb9a234857e29c54506264c451534d
Annalee <annaleeherrera@gmail.com>
2016-04-07 10:57:00 -0700

    returns highest poverty rate for WA

---
+++ povertydata.rb
@@ -0,0 +1,55 @@
+class County
+  attr_accessor :county, :state, :percent, :child_poverty_count, :median_income
+
+  def initialize(county, state, percent, child_poverty_count, median_income)
+    @county = county
+    @state = state
+    @percent = percent
+    @child_poverty_count = child_poverty_count
+    @median_income = median_income
+  end
+
+  def self.all
+    @file_WA= File.open('est14_WA.txt')
+    @file_CA= File.open('est14_CA.txt')
+    array = []
+    @file_WA.each do |row|
+      array.push(
+      County.new(row[193..237], row[239..241], row[76..79], row[49..56], row[133..138]))
+      end
+    return array
+  end
+
+  def self.highest_poverty_WA
+    percents = []
+    all.each do |row|
+      percents.push(row.percent)
+    end
+    return percents.max
+  end
+  puts self.highest_poverty_WA
+end
+
+
+
+#   def self.highest_poverty_CA
+#     poverty_rate_CA = []
+#     @file_CA.each_line do |line|
+#       poverty_rate_CA << line.slice(76..80)
+#     end
+#     return poverty_rate_CA.max
+#   end
+# end
+#
+# def county_name
+#   each_line = []
+#   @file_WA.each_line do |line|
+#     each_line << line
+#   end
+#   array = each_line[8].split(' ')
+#   puts array[23..25].join(' ')
+# end
+
+# puts county_name
+# puts find_highest_poverty_WA
+# puts find_highest_poverty_CA
