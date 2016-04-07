4f300a2b9ec3f408a117553964226b0de9e529a3
Kelly <kelly_devlin@alumni.brown.edu>
2016-04-07 15:17:19 -0700

    create find_extreme_county method

--- poverty/program.py
+++ poverty/program.py
@@ -3,6 +3,27 @@ print ""
 ca_file = open("california.txt")
 wa_file = open("washington.txt")
 
+def find_extreme_county(file):
+    # current_line = file.readline()
+    target_line = file.readline()
+    target_pov_percent = float(target_line.split()[11])
+
+    for line in file:
+        line_pov_percent = float(line.split()[11])
+        if (line_pov_percent > target_pov_percent):
+            target_line = line
+            target_pov_percent = line_pov_percent
+
+    return target_line
+
+# highest_ca_county = find_extreme_county(ca_file, true);
+# lowest_ca_county = find_extreme_county(wa_file, false);
+# highest_wa_county = find_extreme_county(ca_file, true);
+# lowest_wa_county = find_extreme_county(wa_file, false);
+#
+
+# find_extreme_county(ca_file)
+
 ca_file.close()
 wa_file.close()
 # file.close()
