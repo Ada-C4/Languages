598d2628d9f36dc71f75aff91b8bd96702a13959
Rebecca Tolmach <rmtolmach@gmail.com>
2016-04-07 12:20:50 -0700

    search by county working

--- poverty/poverty.rb
+++ poverty/poverty.rb
@@ -2,7 +2,7 @@ class County
   attr_accessor :name, :percent, :num, :median_income
 
   def initialize(name, percent, num, median_income)
-    @name = name
+    @name = name.strip
     @percent = percent.to_f
     @num = num.to_i
     @median_income = median_income.to_i
@@ -21,6 +21,16 @@ class County
       data_array.push(County.new(line[193..217], line[76..79], line[49..56], line[133..138]))
     end
     return data_array
+  end
+
+  def self.find_by_county
+    print "Search by counties in #{@state}. Type in a county: "
+    county_input = gets.chomp.downcase
+    all_counties.each do |county|
+      if county.name.strip.downcase == county_input
+        puts "There is #{county.percent}% poverty in #{county.name}. That amounts to #{county.num} people. The median income in #{county.median_income}"
+      end
+    end
   end
 
   def self.find_highest_pov
@@ -55,6 +65,7 @@ class County
     puts "#{lowest}% poverty in #{county_name} is the lowest in #{@state}. #{count} people live in poverty and the median income is $#{income}"
   end
 
-  self.find_highest_pov
-  self.find_lowest_pov
+  self.find_by_county
+  # self.find_highest_pov
+  # self.find_lowest_pov
 end
