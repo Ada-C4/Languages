e4ea2cba39b4b5cfc6a5225303c57913588861ad
Kelly <kelly_devlin@alumni.brown.edu>
2016-04-07 10:10:55 -0700

    rename scanner variables

--- poverty/src/poverty/Program.java
+++ poverty/src/poverty/Program.java
@@ -7,33 +7,33 @@ import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {	
 		System.out.println("Welcome to the CA/WA Poverty Educator!");
-		Scanner scan = new Scanner(System.in);
+		Scanner scanInput = new Scanner(System.in);
 
 		File file = null; 
 		String state = null;
 		while(file == null) {
 			// capture user input for state 
 			System.out.print("Enter 'CA' for California or 'WA' for Washington: ");
-			state = scan.nextLine().toUpperCase();
+			state = scanInput.nextLine().toUpperCase();
 			// use appropriate file based on user state selection
 			if(state.equals("WA")) {
 				file = new File("washington.txt");
 			} else if(state.equals("CA")){
 				file = new File("california.txt");
 			}
-		}	
+		}
 		
 		try {
-			Scanner scanner = new Scanner(file);
+			Scanner scanFile = new Scanner(file);
 			// capture user input for county 
 			System.out.print("Enter a county in " + state + ": ");
-			String county = scan.nextLine();
-			scan.close();
+			String county = scanInput.nextLine();
+			scanInput.close();
 			// find and print the line matching the county entered by user
 			String line; 
 			boolean countyFound = false;
-			while (scanner.hasNextLine()) {
-				line = scanner.nextLine();
+			while (scanFile.hasNextLine()) {
+				line = scanFile.nextLine();
 				
 				if(line.toLowerCase().contains(county.toLowerCase())) { 		            
 		            County userCounty = new County(line);
@@ -44,7 +44,7 @@ public class Program {
 		            countyFound = true;
 		        }
 			}
-		    scanner.close();
+			scanFile.close();
 		    if(!countyFound) {
 		    	System.out.println("Invalid county for " + state);
 		    }
