d9858687a39af77e457320efed00dfa2660734a2
Annalee <annaleeherrera@gmail.com>
2016-04-07 15:32:15 -0700

    logic for lowest data, need to figure out how to return data

--- povertydata.rb
+++ povertydata.rb
@@ -28,11 +28,31 @@ class County
   def self.highest_poverty_WA
     percents = []
     all.each do |row|
-      percents.push(row.percent += row.county)
+      percents.push(row.percent += row.county += row.child_poverty_count += row.median_income)
     end
     return percents.max
   end
+
+
+
+  def self.lowest_poverty_WA
+    percents = []
+    all.each do |row|
+      percents.push(row.percent += row.county += row.child_poverty_count += row.median_income)
+    end
+    return percents.min
+  end
+  puts self.lowest_poverty_WA
+end
+
+def user_interaction
+  puts "Enter poverty statistic"
+  stat = gets.chomp.downcase
+  if stat == higher
     puts self.highest_poverty_WA
+  else
+    puts self.lowest_poverty_WA
+  end
 end
 
 #   def self.counties
