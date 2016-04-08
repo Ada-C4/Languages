3620c122576dd1b1174f919cf169076b093952d4
emgord <emgord@gmail.com>
2016-04-08 10:14:54 -0700

    created CountyData class

--- povertydata.py
+++ povertydata.py
@@ -2,6 +2,20 @@
 #
 # script, filename = argv
 
+class CountyData:
+    def __init__(self, children_count, children_percentage, median_income, county_name):
+        self.children_count = children_count
+        self.children_percentage = children_percentage
+        self.median_income = median_income
+        self.county_name = county_name
+
+    def print_data(self):
+        print  self.county_name
+        print 'Percentage of Children in Poverty: %s' % self.children_percentage
+        print 'Number of Children in Poverty: %s' % self.children_count
+        print 'Median Household Income: %s' % self.median_income
+
+
 print "Welcome to the US Poverty Data Portal"
 print "Enter WA to view poverty data for Washington or OR to view poverty data for Oregon:"
 state = raw_input("> ")
@@ -13,6 +27,7 @@ elif state == "OR":
 else:
     print "Invalid input"
 
-raw_state_data = open(state_file_path)
+raw_state_data = open(state_file_path).read()
+raw_state_data_list = raw_state_data.split('\n')
 
-print raw_state_data.read()
+# print raw_state_data_list
